42db1bab1c6e3a32d20f7e986eff7068c2497751
Eugenie94 <engie94400@gmail.com>
2021-01-06 23:52:50 +0100

    e

--- index.php
+++ index.php
@@ -29,9 +29,7 @@
         <span class="phrase-qualite tx">La qualité, mon métier.</span>
       </div>
     </div>
-      <!-- <span class="titre" data-in-effect="fadeInDown" data-out-effect="fadeOutDown">Eugénie KHAYAT</span>
-      <span class="sous-titre titre"> Développeuse Web FullStack</span>
-      <span class="phrase-qualite titre">La qualité, mon métier</span> -->
+
 <!--        A PARTIR DE LA C EST MON ANCIENNE VERSION AVEC LES MEDIA QUERIES QUI SE TROUVE DANS MEDIA QUERIES.CSS
    <div class="row">
     <div class="col-lg-12">
---
+++ page1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php require 'head.php'; ?>
+
+  <body>
+
+<?php require 'header.php'; ?>
+
+  <div class="fondimagetexte">
+  <iframe src="CV_Développeuse_Web_Fullstack.pdf" width="100%" height="1300">
+  </iframe>
+  <iframe class='ldm' src="lettre_de_motivation.pdf" width="100%" height="1300">
+  </iframe>
+  <div class="container is-widescreen">
+  <h2>Mon plan de carrière</h2>
+  <p>Je souhaiterai devenir une Développeuse Web accomplie et reconnue. J'aime ce métier par dessus tout et je mettrai
+    toutes les chances de mon côtés pour que mon projet aboutisse. J'ai un tempérament de leadeuse lorsque je travaille en groupe et
+    j'ai une parfaite maîtrise de Trello afin de détailler le projet et d'attribuer une ou plusieurs tâches aux personnes de mon groupe. J'ai un plan de carrière bien défini pour l'avenir :
+     Je m'apprête a démarrer une formation dans le numérique qui se nomme "Social Builder" le 9 Novembre 2020. Cette formation se divise entre trois mois d'école et trois mois d'entreprise.
+    Je donnerai mon meilleur pour convaincre mon futur patron de stage de m'embaucher dans son équipe.
+  </p>
+  </div>
+
+
+<?php require 'footer.php'; ?>
+
+  </body>
+</html>
